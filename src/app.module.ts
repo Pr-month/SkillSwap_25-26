b2f7f4c1a8b1f58449f785ebc6fe6c511d5c576b
goentry <glebyudanov1@yandex.ru>
2025-08-24 13:28:54 +0500

    Подключение к TypeORM(Postgres)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'skillswap',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     JwtModule.register({
       global: true,
     }),
