93aea08589b37f29cec602cac2e4a4be81c78f57
Liza Bragina <catherineby123@yandex.ru>
2025-09-03 19:53:07 +0300

    получение данных пользовтеля по эндпоинту get

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getMe(@Request() req) {
+    return this.usersService.findById(req.user.id);
+  }
 }
