41de4533a9a3ef2f3e66329f2a24c890f7447837
konopat <k0n0pat@yandex.ru>
2025-09-11 17:08:28 +0800

    feat/config: Добавить модуль с типизацией конфига

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+// Экспорт всех конфигураций
+export { appConfig } from './app.config';
+export { jwtConfig } from './jwt.config';
+export { databaseConfig } from './database.config';
+
+// Экспорт типов
+export type { IAppConfig, IJwtConfig, IDatabaseConfig } from './config.types';
