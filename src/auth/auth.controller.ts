17fdd2cfa3a0f6750d1546ef666cdf8bc795c9af
Roland <machodin@yandex.ru>
2025-09-12 17:43:37 +0300

    hotfix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Post, Body, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
-import { TokensDto } from './dto/tokens.dto';
 import { RegisterDto } from './dto/register.dto';
+import { TokensDto } from './dto/tokens.dto';
 import { AuthenticatedRequest, LoginDto } from './interfaces/auth.interface';
-import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -23,10 +22,6 @@ export class AuthController {
   @Post('refresh')
   async refresh(@Body() refreshTokenDto: RefreshTokenDto): Promise<TokensDto> {
     return this.authService.refreshTokens(refreshTokenDto);
-  }
-  @Post('register')
-  async register(@Body() createUserDto: CreateUserDto) {
-    return this.authService.register(createUserDto);
   }
 
   @Post('logout')
