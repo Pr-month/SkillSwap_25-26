8122e12ab5c745e82717b8662f1945d8b1582123
konopat <k0n0pat@yandex.ru>
2025-09-15 19:05:28 +0800

    feat/users: Исключить пароль из ответов API и добавить константы для селектов

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,10 +1,28 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Exclude } from 'class-transformer';
 import { Gender, UserRole } from '../enums';
 // import { Skill } from './skill.entity';
 // import { Category } from './category.entity';
 
 @Entity('users')
 export class User {
+  // Константы для селектов полей (статические для переиспользования)
+  static readonly SELECT_FIELDS: (keyof User)[] = [
+    'id',
+    'name',
+    'email',
+    'about',
+    'birthdate',
+    'city',
+    'gender',
+    'avatar',
+    'role',
+  ];
+
+  static readonly SELECT_WITH_PASSWORD: (keyof User)[] = [
+    ...User.SELECT_FIELDS,
+    'password',
+  ];
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
@@ -15,6 +33,7 @@ export class User {
   email: string;
 
   @Column({ type: 'varchar' })
+  @Exclude() // Исключаем пароль из всех ответов API
   password: string;
 
   @Column({ type: 'text', nullable: true })
