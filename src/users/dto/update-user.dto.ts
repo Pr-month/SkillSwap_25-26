e3abc65ab20501158d3705cfb19715ab789da603
goentry <glebyudanov1@yandex.ru>
2025-09-21 17:14:03 +0500

    DTO для обновления текущего пользователя

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,9 +1,8 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, OmitType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString, IsEmail, IsDate } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  // Наследует все поля из CreateUserDto как опциональные
-  // name?: string;
-  // email?: string;
-  // password?: string; // В будущем можно исключить пароль из обновления
+export class UpdateUserDto extends PartialType(
+  OmitType(CreateUserDto, ['password', 'gender'] as const)
+) {
 }
