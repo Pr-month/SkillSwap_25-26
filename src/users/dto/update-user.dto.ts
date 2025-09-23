db7f3f93bec5ce24f27f34efdb586d4b310bba76
goentry <glebyudanov1@yandex.ru>
2025-09-23 17:10:27 +0500

    Lint fix

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,6 @@
 import { PartialType, OmitType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsOptional, IsString, IsEmail, IsDate } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(
-  OmitType(CreateUserDto, ['password', 'gender'] as const)
-) {
-}
+  OmitType(CreateUserDto, ['password', 'gender'] as const),
+) {}
