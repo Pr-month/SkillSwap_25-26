4176a71a1a945afd3451bcf2cb65828c89b47e6f
Roland <machodin@yandex.ru>
2025-10-12 13:02:57 +0300

    fix for tests

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -12,7 +12,7 @@ export const databaseConfig = registerAs(
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 5432,
     username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || 'password',
+    password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_DATABASE || 'skillswap',
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: process.env.NODE_ENV !== 'production', // Отключаем в продакшене
